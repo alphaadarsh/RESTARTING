1e02db33bea7304de0cfb29a70e5331a3f82cb4c
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-07-31 18:58:42 +0530

    task 3.1

---
+++ question/question_3.py
@@ -0,0 +1,11 @@
+student_marks = float(input("Enter your marks: "))
+if student_marks >=90 : 
+    print("A grade")
+elif student_marks >=80:
+    print("B grade")
+elif student_marks >=70:
+    print("C grade")
+elif student_marks >=60:
+    print("D grade")
+else:
+    print("F grade")
