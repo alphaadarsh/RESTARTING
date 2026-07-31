d5064de3ceb518c3d2a2882435fbd05845101c3c
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-01 00:42:08 +0530

    learn for loop basics
    this  is for loopbasics

---
+++ forloop.py
@@ -0,0 +1,16 @@
+#  for loop =>  repeatedly execute a block of code for a fixed number of times. In python, we have two types of loops: for loop and while loop. and used most of times  
+
+# lets talk about for loop in python. for loop is used to repeatedly execute a block of code for a fixed number of times. the syntax of a for loop is as follows:
+
+# num =  range (10)  # this will create a range object that represents the numbers from 0 to 9.
+for i in range(1 , 10):  # this will iterate over the range object and assign the value of each element in the range to the variable i.
+    print(i)  # this will print the value of i in each iteration of the loop.ckea
+
+print ("new code")  # this will print the string "Hello, World!" after the for loop has completed.
+
+for j in range(1 ,11):  # this will iterate over the range object and assign the value of each element in the range to the variable j.
+        if  j % 2 == 0:  # this will print the value of i multiplied by j in each iteration of the loop.
+            print(j)
+
+for k in range (2 , 11 , 2) :  # this will iterate over the range object and assign the value of each element in the range to the variable k.
+    print(k)  # this will print the value of k in each iteration of the loop.
