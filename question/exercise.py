8b7b33bd7a73648b2cc84ecf41da084afd54486e
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-02 01:50:23 +0530

    exercise
    i solve 3  question which is logical based

---
+++ question/exercise.py
@@ -0,0 +1,14 @@
+# print all odd number from 1 to 20 oddd number which have remainder 1 when divided by 2
+for i in range (1, 21):
+    if i % 2 != 0:
+        print(i)
+print ("code end ")
+        # print tge table of 57 
+for j in range (1 , 11) : 
+    print (57 * j)  # this will print the value of 57 multiplied by j in each iteration of the loop.
+print ("code end ")
+for k in range (1 , 50) :
+    if (k == 15): 
+        continue  # this will skip the current iteration of the loop if the condition is true and continue with the next iteration of the loop.
+    if k % 3 == 0 :  # this will check if the value of k is divisible by 3 in each iteration of the loop.
+        print (k)  # this will print the value of k in each iteration of the loop.
