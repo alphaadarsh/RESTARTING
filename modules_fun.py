373716268f39f4efdc9ff8caea225d447b60819c
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-02 03:50:03 +0530

    Add modules_fun.py with math and random module examples

---
+++ modules_fun.py
@@ -0,0 +1,6 @@
+#  modules  
+import math
+import random  # this will import the math module which contains mathematical functions and constants.
+print(dir(math))  # this will print the list of all the functions and constants in the math module.
+
+print (random.random () )  # this will print a random float between 0.0 and 1.0.
