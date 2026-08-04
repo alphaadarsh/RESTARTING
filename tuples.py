abd4c028b190e06188c35a0542a20bf1a9b43bc2
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-04 22:51:02 +0530

    Remove unused files: sets.py, setup.py, tuples.py, and whileloop.py

--- tuples.py
+++
@@ -1,5 +0,0 @@
-#  tiple ------------  immutable data type in python
-
-marks =  (90, 80, 70, 60, 50 , 95 ,95 ,95 )  # this will create a tuple marks with the elements 90, 80, 70, 60, and we can also not use paranthesis to create a tuple but it is recommended to use parentheses for clarity and readability.
-print (marks ,type(marks) , marks[2] , marks.index(70) )  # this will print the tuple marks and its type. 
-
