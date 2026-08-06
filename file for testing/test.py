0159920e5a8f3253361f794f7439565e7c2996c3
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-06 23:03:43 +0530

    Refactor user input prompts and output message in test.py

--- file for testing/test.py
+++ file for testing/test.py
@@ -1,3 +1,8 @@
 # put your python code here
-data = input()
-print(data) 
+# data = input()
+# print(data) 
+
+name = input("Please enter your name: ") #prompt for user's name
+age = int(input("Please enter your age: ")) #prompt for user's age
+year_born = 2025 - age #calculates birth year
+print(f"Hello {name}, you were probably born in {year_born}.") #prints out formatted personal messageadarsh
