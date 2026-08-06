9006c6c88052a232ac68c4f998475e952304873a
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-06 23:22:22 +0530

    Comment out user input prompts and calculations; add functionality to print user input twice

--- file for testing/test.py
+++ file for testing/test.py
@@ -2,7 +2,12 @@
 # data = input()
 # print(data) 
 
-name = input("Please enter your name: ") #prompt for user's name
-age = int(input("Please enter your age: ")) #prompt for user's age
-year_born = 2025 - age #calculates birth year
-print(f"Hello {name}, you were probably born in {year_born}.") #prints out formatted personal messageadarsh
+# name = input("Please enter your name: ") #prompt for user's name
+# age = int(input("Please enter your age: ")) #prompt for user's age
+# year_born = 2025 - age #calculates birth year
+# print(f"Hello {name}, you were probably born in {year_born}.") #prints out formatted personal messageadarsh
+
+print("Enter a number: ")
+user_num = input()
+# user enters 10
+print(user_num + user_num)
