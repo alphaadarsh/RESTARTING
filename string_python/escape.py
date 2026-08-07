5c6cdff8b724dad554b0fb397ff32b7102092b85
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-07 08:40:25 +0530

    Add escape sequences examples in escape.py for string formatting demonstration

---
+++ string_python/escape.py
@@ -0,0 +1,12 @@
+# lets learn  \  
+#  \n next line 
+#  \t tab space
+# \\ backslash
+# \' single quote inside string wiith single quote
+# \" double quote inside string with double quote
+
+print("Hello\nWorld")  # Output: Hello (next line) World    
+print("Hello\tWorld")  # Output: Hello (tab space) World
+print("Hello\\World")  # Output: Hello\World
+print('It\'s a beautiful day.')  # Output: It's a beautiful day.
+print("She said, \"Hello!\"")  # Output: She said, "Hello!"
