d6947026573c7a34d460a3c97ae22bcee8ddcf9c
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-07 08:45:22 +0530

    Add operation_str.py to demonstrate string operations in Python

---
+++ string_python/operation_str.py
@@ -0,0 +1,13 @@
+#  lets learn operation on string 
+
+s1  = "hello python is fun "
+s2  = " lets learn operation on string "
+print (s1+s2)  # Output: hello python is fun  lets learn operation on string
+
+s3  = "hello python is fun "
+print (s3*3)  # Output: hello python is fun hello python is fun
+
+#   membrship operator in python is used to check if a substring is present in the string or not.
+s4  = "hello python is fun "
+print("python" in s4)  # Output: True
+print("Java" in s4)  # Output: False
