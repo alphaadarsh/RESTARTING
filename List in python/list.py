30d6f7be48b9640d720cf4e6f62a0a5d0b2722fb
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-07 11:25:41 +0530

    Add initial list.py file to document list data structure in Python

---
+++ List in python/list.py
@@ -0,0 +1,4 @@
+# list is data structure in python which is used to store multiple items in a single variable. It is one of the most commonly used data structures in python. Lists are ordered, changeable, and allow duplicate values. They are defined by enclosing elements in square brackets [].
+
+#  can we store different data types in a list?  yes we can store different data types in a list.
+#  in single list we can store different data types like int, float, string, boolean, list, tuple, set, dictionary etc.
