cd3892251085b69ce11ae1e5405640c7dd2cd238
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-10 22:42:41 +0530

    Add births_year.py to calculate and display user's birth year and age information

---
+++ git commit/births_year.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+next_year_age = age + 1
+birth_year = 2026 - age
+
+print("Hello,", name)
+print("You are", age, "years old.")
+print("Next year you will be", next_year_age)
+print("Your approximate birth year is", birth_year)
