5f280d583c4e3f5c43540c33e697ea2e934468ab
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-10 22:37:10 +0530

    Add calculateage.py to prompt user for name and age, and display current and next year age

---
+++ git commit/calculateage.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+next_year_age = age + 1
+
+print("Hello,", name)
+print("You are", age, "years old.")
+print("Next year you will be", next_year_age)
