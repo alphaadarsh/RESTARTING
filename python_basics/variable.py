dab2dd46782a96e6953c3e514fdcd374551c6d7d
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-14 08:33:54 +0530

    Add variable.py to demonstrate string concatenation and formatted strings

---
+++ python_basics/variable.py
@@ -0,0 +1,9 @@
+first_name = "John"
+last_name = "Doe"
+
+print("Full Name: " + first_name + " " + last_name)  # Output: Full Name: John Doe
+print(f"Full Name: {first_name} {last_name}")  # Output: Full Name: John Doe
+
+food = "Pizza"
+print("I love " + food)  # Output: I love Pizza
+print(f"I love {food}")  # Output: I love Pizza
