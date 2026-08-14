7582bdccdc333cd70a69e7393a52a84460e9cf46
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-14 12:02:47 +0530

    Add math module examples and circumference calculation

--- python_basics/basics.py
+++ python_basics/basics.py
@@ -92,3 +92,17 @@ result  =  pow(x, z)
 result = abs(x) # absolute value
 
 print(x)
+
+
+import math  
+
+print(math.pi) # 3.141592653589793
+print(math.sqrt(16)) # 4.0
+print(f"{math.e:.2f}") # 2.718281828459045
+
+result = math.ceil(3.2) # 4
+result = math.floor(3.8) # 3
+
+radius =  float(input("Enter the radius of the circle: "))
+circumference = 2 * math.pi * radius
+print(f"The circumference of the circle is: {circumference}")
