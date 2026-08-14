8992e6ac14e2fdf57a37857ae152e7fde3c62e01
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-14 08:38:06 +0530

    Add variable.py to demonstrate variable usage and calculations

--- python_basics/variable.py
+++ python_basics/variable.py
@@ -1,3 +1,5 @@
+# what is variable 
+
 first_name = "John"
 last_name = "Doe"
 
@@ -7,3 +9,10 @@ print(f"Full Name: {first_name} {last_name}")  # Output: Full Name: John Doe
 food = "Pizza"
 print("I love " + food)  # Output: I love Pizza
 print(f"I love {food}")  # Output: I love Pizza
+
+# variables can also be used in expressions and calculations. For example: 
+price = 10
+quantity = 5
+total_cost = price * quantity
+print(f"Total Cost: ${total_cost}")  # Output: Total Cost: $50
+
