0df0a690d6767a950ab4b7fdc2038ff8ec67ca46
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-14 09:27:00 +0530

    rectangle area

---
+++ question/exercise_23.py
@@ -0,0 +1,8 @@
+# area of rec 
+
+length = float(input("Enter the length of the rectangle: "))
+breadth = float(input("Enter the breadth of the rectangle: "))
+
+area = length * breadth
+
+print(f"The area of the rectangle is: {area}cm^2")
