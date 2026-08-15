d9d9bf90b15a0aa11051109cfee5fa920dbcee65
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-15 07:57:37 +0530

    Add circle.py to calculate circumference and demonstrate math functions

---
+++ question/circle.py
@@ -0,0 +1,14 @@
+
+import math  
+
+print(math.pi) # 3.141592653589793
+print(math.sqrt(16)) # 4.0
+print(f"{math.e:.2f}") # 2.718281828459045
+
+result = math.ceil(3.2) # 4
+result = math.floor(3.8) # 3
+
+radius =  float(input("Enter the radius of the circle: "))
+circumference = 2 * math.pi * radius
+print(f"The circumference of the circle is: {circumference:.2f}")
+
