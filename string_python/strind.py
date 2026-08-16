bcf870acb598d9c86fb2717934702fa6c86cd556
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-16 07:48:07 +0530

    credit card prograam

---
+++ string_python/strind.py
@@ -0,0 +1,14 @@
+# INDEXING     START    END  STEP  
+
+# credit_no ="123-4567-8901-2345"
+
+# print(credit_no[0])  # 1
+# print(credit_no[1])  # 2    
+
+# lets make program to cherck last 4 digit   
+credit_no = "123-4567-8901-2345"
+
+print(credit_no[-4:])  # 2345
+
+credit_no2 = "123-4567-8901-2345" 
+print (credit_no2[::-1])  # 5432-1098-7654-321
