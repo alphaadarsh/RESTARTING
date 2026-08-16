f1e97e9ecb348b8f569954b210d4ae941d70c793
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-16 07:37:20 +0530

    conmdition modified

--- python_basics/conditional.py
+++ python_basics/conditional.py
@@ -1,9 +1,26 @@
-#  conditional statement 
+#  conditional statement  a one line statement that allows you to execute a block of code based on a condition. In python we have if, else and elif statements to use conditional statements.
+#  return x condition else y 
 
-age = int(input("Enter your age: "))
-if age >= 18:    # we are using here indentation to define the block of code that will be executed if the condition is true. indentation is important in python as it defines the scope of the code block.
-    print("You are eligible to vote")
-else:
-    print("You are not eligible to vote")  # this is old method  to use else and than if but there is new method to use elif which is more efficient and readable. 
+# age = int(input("Enter your age: "))
+# if age >= 18:    # we are using here indentation to define the block of code that will be executed if the condition is true. indentation is important in python as it defines the scope of the code block.
+#     print("You are eligible to vote")
+# else:
+#     print("You are not eligible to vote")  # this is old method  to use else and than if but there is new method to use elif which is more efficient and readable. 
 
-    
+#  soo the main rule here is x if condition else y  this is the new method to use else and than if but there is new method to use elif which is more efficient and readable.
+
+# num = 5 
+
+# print("positive" if num > 0 else "negative")  # this is new method to use else and than if but there is new method to use elif which is more efficient and readable. 
+
+# num3 = 5 
+# a = 5 
+# a =  7  
+
+# max_num3 = num3 if num3 > a else a  # this is new method to use else and than if but there is new method to use elif which is more efficient and readable.
+# min_num3 = num3 if num3 < a else a  # this is new method to use else and than if but there is new method to use elif which is more efficient and readable.
+
+temp = 20
+
+weather  = "hot" if temp > 25 else "cold"  # this is new method to use else and than if but there is new method to use elif which is more efficient and readable.
+print(f"The weather is {weather}.")  # this is new method to use else and than if but there is new method to use elif which is more efficient and readable.
