680c7c06f3b4c31e7c993536d533cbd71a9b4dbb
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-17 08:23:44 +0530

    tuples updated

--- python_basics/tuples.py
+++ python_basics/tuples.py
@@ -1,5 +1,6 @@
-#  tiple ------------  immutable data type in python
+#  tUple ------------  immutable data type in python
 
 marks =  (90, 80, 70, 60, 50 , 95 ,95 ,95 )  # this will create a tuple marks with the elements 90, 80, 70, 60, and we can also not use paranthesis to create a tuple but it is recommended to use parentheses for clarity and readability.
 print (marks ,type(marks) , marks[2] , marks.index(70) )  # this will print the tuple marks and its type. 
 
+print(dir(marks))  # this will print the list of all the methods available for the tuple object. 
