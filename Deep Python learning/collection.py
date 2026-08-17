e57cd356480a7d373547f296564f7275e453ef14
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-17 08:23:54 +0530

    collection learn

---
+++ Deep Python learning/collection.py
@@ -0,0 +1,21 @@
+# there are too many method you can use in list like to get dir  in list you can use dir() method and to get help you can use help() method. and also we can use index to get the value of the list.
+#  like examples  len , in , 
+
+
+
+
+
+# fruits =  ["apple", "banana", "cherry" ]
+
+# # print(dir(fruits))  # this will print the list of all the methods available for the list object.
+# # print(help(fruits))  # this will print the help documentation for the list object.
+# for fruit in fruits:  # this will iterate over the list of fruits
+#     print(fruit)  # this will print the value of fruit in each iteration of the loop.
+# print(fruits[2])
+# # print(fruits)
+
+#  set  WE CANT ALTER THE VALUE OF SET LIKE WE CAN ALTER THE VALUE OF LIST.  SET IS UNORDERED COLLECTION OF UNIQUE ELEMENTS.  SET IS MUTABLE BUT CANT ALTER THE VALUE OF SET
+fruits =  {"apple", "banana", "cherry" }  
+
+
+
