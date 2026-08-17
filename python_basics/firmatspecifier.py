240653fca84cadb56a24379b15e50be09e000f72
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-17 08:25:01 +0530

    understand format specifier

---
+++ python_basics/firmatspecifier.py
@@ -0,0 +1,10 @@
+# so there many format specifiers available in python. Here are some of them:  like this <, > , ^, +,  .2f  and also , we can also use f-string to format the output in python.
+# and also we can use 2 or 3 format  
+
+price1 = 3.14159 
+price2  = -913.543
+price3 = 12.71828
+
+print(f"Price 1: {price1:.2f}")
+print(f"Price 2: {price2:.2f}")
+print(f"Price 3: {price3:.2f}")
