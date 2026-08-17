5aa7db4f0d460ca7f541f22e088edf5a3b8cbf86
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-17 08:24:06 +0530

    for loop deep understanding

---
+++ Deep Python learning/forloop.py
@@ -0,0 +1,14 @@
+# for loops   you can execute a block of code for a fixed number of times. In python, we have two types of loops: for loop and while loop. and used most of times
+
+for x in reversed(range(1 , 10 ,2)) :  # at x place we can also use counter variable like i, j, k, l etc.  this will iterate over the range object and assign the value of each element in the range to the variable x.  
+    print (x)  # this will print the value of x in each iteration of the loop.
+
+print ("Happy New Year!")  # this will print the string "Hello, World!" after the for loop has completed.
+
+for counter in range(1 , 31):  # at counter place we can also use variable like i, j, k, l etc.  this will iterate over the range object and assign the value of each element in the range to the variable counter.
+    if x == 15:
+        continue  # this will skip the current iteration of the loop if the condition is true and continue with the next iteration of the loop.
+    else:
+        print (counter)  # this will print the value of counter in each iteration of the loop.
+
+# in simple excte blcok of code in  fixed number of times. In python, we have two types of loops: for loop and while loop. and used most of times
