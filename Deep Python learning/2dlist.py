6c3c9aff1fe7b6d0e087820cfa44d491fd88d0c1
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-18 09:16:57 +0530

    2 d list

---
+++ Deep Python learning/2dlist.py
@@ -0,0 +1,19 @@
+# list made up of list is called 2d list. 2d list is a list of lists. 2d list is also called matrix. 2d list is a collection of elements which are arranged in rows and columns. 2d list is 
+# mutable data type in python. 2d list is also called nested list. 2d list is also called array in other programming languages. 2d list is also called table in other programming languages. 2d list is also called grid in other programming languages. 2d list is also called matrix in other programming languages. 2d list is also called dataframe in other programming languages.
+
+fruits = [ "apple", "banana", "cherry", "orange", "grape", "kiwi"],
+vegitables = [ "carrot", "potato", "tomato", "onion", "cabbage", "spinach"],
+meats  = [ "chicken", "beef", "pork", "lamb", "fish", "shrimp"],
+dairy = [ "milk", "cheese", "yogurt", "butter", "cream", "ice cream"],
+grains = [ "rice", "wheat", "corn", "oats", "   barley", "quinoa"],
+beverages = [ "water", "juice", "soda", "tea", "coffee  ", "beer"],
+
+grocery_list = [fruits, vegitables, meats, dairy, grains, beverages]  # this will create a 2d list grocery_list with the elements fruits, vegitables, meats, dairy, grains, and beverages.
+print(fruits)
+print(vegitables)
+print(meats)
+print(dairy)
+print(grains)
+print(beverages)
+print(grocery_list)  # this will print the 2d list grocery_list.  and also we can use index to get the value of the 2d list.
+print(grocery_list, type(grocery_list))  # this will print the 2d list grocery_list and its type. and also we can use index to get the value of the 2d list.]\
