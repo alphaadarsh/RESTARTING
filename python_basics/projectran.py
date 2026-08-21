788172c0c5eb5f57ac7f3320f3a519c3ba8698f8
Adarsh Pandey <alphaadarsh88@gmail.com>
2026-08-21 19:56:54 +0530

    python basic

---
+++ python_basics/projectran.py
@@ -0,0 +1,18 @@
+import fractions
+import random 
+
+# low  = 1 
+# high = 100 
+
+
+# number = random.randint(low , high)
+
+# print(number)
+
+# number_1 = round(random.random(),2)
+
+# print(number_1)
+options = ("rock" , "paper" , "scissors")
+
+option = random.choice(options)
+print(option)
